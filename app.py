4400d4ffd1e8c0058529565c472053f69933f606
Jamil Nagua <jamilnagua@gamil.com>
2019-11-12 18:52:02 +0100

    contador

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("hola mundo")
+veces=0
+ganancia=200  // ganancia siempre es 200
+total=0
+ 
+print ("****")
+print ("inicia")
+print ("****")
+ 
+while veces<=5:
+	print("veces: ",veces,"ganado: ",ganancia)
+	print("total ganado :",total)
+	total=total+ganancia
+	veces=veces+1
+ 
+print("fin")
